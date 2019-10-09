a242e61a8e5e5f9359592fe702837fe8f0f1f66c
sanhyeok <48554033+sanhyeok@users.noreply.github.com>
2019-10-10 00:18:39 +0900

    Add files via upload

---
+++ StudnetManager/StudnetManager/StudnetManager/StudnetManager/DevStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevStyle
+{
+    public static class List
+    {
+        public static List<T> Empty<T>(T typeDescriptor)
+        {
+            return new List<T>();
+        }
+    }
+}
