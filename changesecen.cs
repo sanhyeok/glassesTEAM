55182fd4838ec24159fed26b1a7622f6df32ad36
야옹 <49221790+MerryBad@users.noreply.github.com>
2019-11-28 14:10:42 +0900

    씬 전환 연습.
    LoadScene에 Scene의 이름을 적으면 된다.

---
+++ changesecen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changesecen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            LoadScene("test1");
+        if (Input.GetMouseButtonDown(1))
+            LoadScene("SampleScene");
+    }
+}
