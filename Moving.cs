b02556237544d2f8f154a44e1e90f9497846cb5a
야옹 <49221790+MerryBad@users.noreply.github.com>
2019-11-28 14:11:29 +0900

    행성 공전 스크립트
    오브젝트(태양)의 위치를 원점으로 원운동을 한다

---
+++ Moving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Planet;       //기준행성
+    public float speed;             //회전 속도
+
+    private void Update()
+    {
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(Planet.transform.position, Vector3.down, speed * Time.deltaTime);
+    }
+    // RotateAround(Vector3 point, Vector3 axis, float angle)
+}
