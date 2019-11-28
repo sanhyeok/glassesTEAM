e43cb919bd783b71d7a6c4aac9715d5239eba4e1
야옹 <49221790+MerryBad@users.noreply.github.com>
2019-11-28 14:44:28 +0900

    행성 자전 스크립트
    돈다..

---
+++ SelfMoving.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;        //회전속도
+    private void Update()
+    {
+        Orbit_Rotation();
+    }
+
+    void Orbit_Rotation()
+    {
+        transform.Rotate(Vector3.down * speed * Time.deltaTime);
+        //transform.Rotate(Vector3 EularAngle)
+    }
+}
