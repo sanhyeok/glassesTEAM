ead50382f17c253e4bdfc90c533b5df65222b61a
야옹 <49221790+MerryBad@users.noreply.github.com>
2019-11-29 17:18:16 +0900

    Update changesecen.cs

--- changesecen.cs
+++ changesecen.cs
@@ -1,4 +1,18 @@
-﻿using System.Collections;
+/***********************************
+
+* 프로그램명 : changesecen.cs
+
+* 작성자 : 정선우
+
+* 작성일 : 2019년 11월27일
+
+* 프로그램 설명 : 조건에 맞게 실행하고 있는 secen을 바꾼다.
+                 맵 이동에 사용.
+
+***********************************/
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
