71e159b9e3ed581c82758e73f0f83ca229548a57
야옹 <49221790+MerryBad@users.noreply.github.com>
2019-11-30 21:06:40 +0900

    목적지에 도착시 다음 인덱스의 씬 로드
    다른 씬의 목적지를 어떻게 받지..

--- changesecen.cs
+++ changesecen.cs
@@ -5,6 +5,7 @@
 * 작성자 : 정선우
 
 * 작성일 : 2019년 11월27일
+* 수정일 : 2019년 11월30일
 
 * 프로그램 설명 : 조건에 맞게 실행하고 있는 secen을 바꾼다.
                  맵 이동에 사용.
@@ -18,21 +19,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class changesecen : MonoBehaviour
 {
+    private int Scene_index = 0;
+    public List<GameObject> dest; // 목적지
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-    public void LoadScene(string name)
+    public void LoadScene(int index)
     {
-        SceneManager.LoadScene(name);  // name에 해당하는 scene을 호출한다.
+        SceneManager.LoadScene(index);  // index에 해당하는 scene을 호출한다.
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))  // 좌클릭을 입력받으면
-            LoadScene("test1");           // test1이라는 씬을 호출
-        if (Input.GetMouseButtonDown(1))  // 우클릭을 입력받으면
-            LoadScene("SampleScene");     // SampleScene을 
+        if (Vector3.Distance(this.transform.position, dest[Scene_index].transform.position) < 10)  // 목적지에 다다르면
+        {
+            Scene_index += 1;
+            LoadScene(Scene_index);
+        }// 다음 인덱스의 씬으로 이동
     }
 }
