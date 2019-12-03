19ba688817a6aed3b94064a0b68014a425d9bc99
sanhyeok <48554033+sanhyeok@users.noreply.github.com>
2019-12-04 00:47:41 +0900

    Add files via upload

---
+++ mycube.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mycube : MonoBehaviour
+{
+    public float speed = 3f;
+    public float jumpPower = 3f;
+    bool isJumping;
+    float horizon;
+    float vertical;
+
+    Rigidbody rigdbody;
+    Vector3 movement;
+    void Awake()
+    {
+        rigdbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        horizon = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+            isJumping = true;
+    }
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+    void Run()
+    {
+        movement.Set(horizon, 0, vertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rigdbody.MovePosition(transform.position + movement);
+    }
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+        //rigdbody.MovePosition(transform.position + Vector3.up);
+        rigdbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+        isJumping = false;
+    }
+
+}
